e60f3e398c7461e126ae8f82175c2fb8b4bd3b62
SlowRiot <slowriot@voxelstorm.com>
2023-01-30 12:26:10 +0000

    show simple and advanced usage in example

--- example.cpp
+++ example.cpp
@@ -6,9 +6,11 @@ auto main()->int {
   std::string const my_buffer{"Hello world!\n"};
   std::ofstream stream("my_tarball.tar", std::ios::binary);
 
+  tar_to_stream(stream, "hello_world_basic.txt", my_buffer.data(), my_buffer.size()); // add one file to the archive
+
   tar_to_stream(
     stream,                              // the stream to write to
-    "my_filename.txt",                   // filename (string)
+    "hello_world_advanced.txt",          // filename (string)
     my_buffer.data(),                    // pointer to buffer data (file contents)
     my_buffer.size(),                    // size of buffer data (file contents)
     static_cast<uint64_t>(std::time(0)), // file modification time (mtime): this sets it to "now"
@@ -18,6 +20,8 @@ auto main()->int {
     "my_username",                       // file owner username
     "my_group"                           // file owner group name
   );
+
+  tar_to_stream_tail(stream); // finalise the archive by adding a tail of zeros
 
   return EXIT_SUCCESS;
 }
